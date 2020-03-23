aa02cd073ed6bca73839ee661fc51715fcceb270
kwunodong <kwunodong@gmail.com>
2020-03-24 08:52:36 +0900

    3/24 김보성LG그램 Master 동기화중 강제커밋 요구로 커밋실시(pull,패치 모두 안됨)

--- ProjectProgram/SpringSever/Binder/src/main/java/net/softsociety/binder/HomeController.java
+++ ProjectProgram/SpringSever/Binder/src/main/java/net/softsociety/binder/HomeController.java
@@ -1,31 +1,17 @@
 package net.softsociety.binder;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestParam;
-
-import net.softsociety.binder.dao.BoardDAO;
 import net.softsociety.binder.util.PageNavigator;
-import net.softsociety.binder.vo.Board;
-=======
->>>>>>> 브랜치_보성
-
-
 @Controller
 public class HomeController {
-	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	
-
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
 		logger.info("메인페이지 시작");
 		return "index";
 	}
-	
 }
