fb1bcffe4126b26ad4774c50b752265fa1196fdf
JEGON SUHWAN <jsh4646@naver.com>
2020-04-29 10:25:49 +0900

    제발 마지막 커밋이 되기를

--- ProjectProgram/SpringSever/Binder/src/main/java/net/softsociety/binder/dao/HashTagMapper.java
+++ ProjectProgram/SpringSever/Binder/src/main/java/net/softsociety/binder/dao/HashTagMapper.java
@@ -1,6 +1,7 @@
 package net.softsociety.binder.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import net.softsociety.binder.vo.HashTag;
 
@@ -10,4 +11,8 @@ public interface HashTagMapper {
 	public ArrayList<HashTag> selectHashTags(String member_id);
 	
 	public int insertHash(HashTag hashtag);
+	
+	public ArrayList<HashMap<String,Object>> hashRankNow();
+
+	public ArrayList<HashMap<String,Object>> hashRankMonth();
 }
